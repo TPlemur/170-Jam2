ef9977be4475069d75ecceda3ae87d2b569cc806
jontyjlewis <jontyjlewis@gmail.com>
2022-11-01 17:48:21 -0700

    added rest of the room details, made datapad shell

--- Assets/scripts/openDoor.cs
+++ Assets/scripts/openDoor.cs
@@ -4,31 +4,9 @@ using UnityEngine;
 
 public class openDoor : MonoBehaviour
 {
-    public GameObject doorLeft;
-    public GameObject doorRight;
-
     private bool isOpen = false;
 
-    private Vector3 doorLeftStart;
-    private Vector3 doorRightStart;
-
-    private Vector3 doorLeftEnd;
-    private Vector3 doorRightEnd;
-
     public Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        doorLeftStart = doorLeft.transform.position;
-        doorRightStart = doorRight.transform.position;
-
-        doorLeftEnd = doorLeftStart;
-        doorLeftEnd.x += -4;
-
-        doorRightEnd = doorRightStart;
-        doorRightEnd.x += 4;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
