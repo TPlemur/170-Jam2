f6dd033fcfcd67bf1ad49e27a343274939bbbd38
TPlemur <tplemur@gmail.com>
2022-11-01 18:18:24 -0700

    implemented portal switching

--- Assets/scripts/PortalScripts/PortalManager.cs
+++ Assets/scripts/PortalScripts/PortalManager.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public class PortalManager : MonoBehaviour
 {
-    public Portal[] MasterPortals;
-    public Portal[] SlavePortals;
+    public Portal[] telaPortals;
+    public openDoor[] controlDoors;
+
+    public float portalHeight = 0.99f;
+    public float portalStorage = 20f;
 
     Portal currentPortal;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set portal to be present at start of game
+        currentPortal = telaPortals[0];
+        currentPortal.transform.position = new Vector3(currentPortal.transform.position.x, portalHeight, currentPortal.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < SlavePortals.Length; i++)
+        for (int i = 0; i < telaPortals.Length; i++)
         {
-            if (SlavePortals[i].canSee)
+            //switch to portal on door opening
+            if (controlDoors[i].isNowOpen())
             {
-                currentPortal = SlavePortals[i];
-                MasterPortals[i].transform.position = new Vector3(MasterPortals[i].transform.position.x, 0.91f, MasterPortals[i].transform.position.z);
+                currentPortal = telaPortals[i];
+                telaPortals[i].transform.position = new Vector3(telaPortals[i].transform.position.x, portalHeight, telaPortals[i].transform.position.z);
             }
-        }
-        for (int i = 0; i < SlavePortals.Length; i++)
-        {
-            if( SlavePortals[i] != currentPortal)
+            //store unused portals
+            if(telaPortals[i] != currentPortal)
             {
-                MasterPortals[i].transform.position = new Vector3(MasterPortals[i].transform.position.x, 20f, MasterPortals[i].transform.position.z);
+                telaPortals[i].transform.position = new Vector3(telaPortals[i].transform.position.x, portalStorage, telaPortals[i].transform.position.z);
 
             }
         }
--- Assets/scripts/openDoor.cs
+++ Assets/scripts/openDoor.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class openDoor : MonoBehaviour
 {
+    public GameObject doorLeft;
+    public GameObject doorRight;
+
     private bool isOpen = false;
 
+    public bool isNowOpen() { return isOpen; }
+
+    private Vector3 doorLeftStart;
+    private Vector3 doorRightStart;
+
+    private Vector3 doorLeftEnd;
+    private Vector3 doorRightEnd;
+
     public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorLeftStart = doorLeft.transform.position;
+        doorRightStart = doorRight.transform.position;
+
+        doorLeftEnd = doorLeftStart;
+        doorLeftEnd.x += -4;
+
+        doorRightEnd = doorRightStart;
+        doorRightEnd.x += 4;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
