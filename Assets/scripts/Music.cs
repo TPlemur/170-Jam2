a4f7dafbd2c9c05c1555c887d57c53ac76928830
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2022-11-01 18:48:23 -0700

    added gravity to button switch and bgm

---
+++ Assets/scripts/Music.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
