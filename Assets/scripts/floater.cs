ce718315d52a01f8ba53a7f4afe1f33dea4f536e
TPlemur <tplemur@gmail.com>
2022-11-03 12:16:00 -0700

    artifact cube rotation script

--- Assets/scripts/floater.cs
+++ Assets/scripts/floater.cs
@@ -6,15 +6,19 @@ public class floater : MonoBehaviour
 {
     public float amplitude = 0.5f;
     public float frequency = 1f;
+    public GameObject cube;
 
     // Position Storage vars
     Vector3 posOffset = new Vector3();
     Vector3 tempPos = new Vector3();
 
+    Transform cubeTF;
+
     // Start is called before the first frame update
     void Start()
     {
         posOffset = transform.position;
+        cubeTF = cube.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,7 @@ public class floater : MonoBehaviour
         // float up/down with a sin()
         tempPos = posOffset;
         tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        cubeTF.Rotate(0, 0.5f, 0.6f);
 
         transform.position = tempPos;
     }
